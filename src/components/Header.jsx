9409830e83fbb1dd796294cf63a2bfc29c641aa5
mthunzi <mthunzi99@gmail.com>
2025-04-15 11:40:45 +0200

    Header component completed for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,24 @@ import { navigation } from "../constants/index.js";
 
 export const Header = () => {
   const pathname = useLocation();
+
   return (
-    <div className="fixed top-0 left-0 right-0 bg-white shadow-md p-4 z-10 backdrop-blur-sm">
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a href="#mtl" className="block w-[12rem] xl:mr-8">
+    <div className="fixed top-0 left-0 right-0 bg-amber-400/90 shadow-md p-3 z-10 backdrop-blur-sm">
+      <div className="flex items-center px-4 lg:px-7.5 xl:px-10 max-lg:py-3">
+        <a href="#hero" className="block w-[12rem] xl:mr-8">
           <img src={mtlLogo} alt="MTL" width={190} height={20} />
         </a>
-
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-amber-200 lg:static lg:flex lg:mx-auto lg:bg-transparent">
-          <div className="relative z-2 flex flex-col items-center m-auto lg:flex-row">
+        <nav classname="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-amber-400 md:static md:flex md:mx-auto md:bg-transparent">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto md:flex-row">
             {navigation.map((item) => (
               <a
                 key={item.id}
-                href={`#${item.url}`}
-                className={`block px-4 py-2 text-lg text-gray-700 transition-colors hover:text-gray-900 lg:mr-6 lg:p-0 ${
-                  item.url === pathname.hash
-                    ? "z-2 lg:text-red-900"
-                    : "lg:text-gray-700"
-                }`}
+                href={item.url}
+                className={`block relative text-2xl uppercase text-gray-700 transition-colors hover:text-blue-900 mx-3 px-2 py-4 md:my-8 lg:-mr-0.25 md:text-sm ${
+                  pathname.hash === item.url
+                    ? "text-blue-900 font-bold border-b-4 border-blue-900"
+                    : ""
+                } md:leading-4`}
               >
                 {item.title}
               </a>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export const navigation = [
-  { id: "0", title: "Services", url: "Services" },
-  { id: "1", title: "About", url: "About" },
+  { id: "0", title: "Home", url: "#Home" },
+  { id: "1", title: "Services", url: "#Services" },
+  { id: "1", title: "About", url: "#About" },
 ];
